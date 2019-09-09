6a1d16b17dc6f16d1ce97a3e959c629dc0ac670d
Muyinda Rogers <moverr@gmail.com>
2019-09-09 09:14:40 +0200

    first commit

---
+++ partyInvites/Controllers/B2Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace partyInvites.Controllers
+{
+    public class B2Controller : Controller
+    {
+        // GET: B2
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ partyInvites/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using partyInvites.Models;
+using System;
+using System.Web.Mvc;
+
+namespace partyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        /*public ActionResult Index()
+        {
+            return View();
+        }
+        */
+        public ViewResult index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon" ;
+            return View();
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult RsvForm()
+        {
+            ViewBag.Message = "Your contact page.";
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult RsvForm(GuestResponse guestResponse)
+        {
+            ViewBag.Message = "Your contact page.";
+            return View();
+        }
+
+
+    }
+}
