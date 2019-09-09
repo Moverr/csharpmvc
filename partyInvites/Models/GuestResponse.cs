09a9e266e1ff7edcc46b2d150e79f7fe5da7bf09
Muyinda Rogers <moverr@gmail.com>
2019-09-09 17:06:39 +0200

    :Debugging :

--- partyInvites/Models/GuestResponse.cs
+++ partyInvites/Models/GuestResponse.cs
@@ -6,18 +6,24 @@ using System.Web;
 
 namespace partyInvites.Models
 {
+
+    //We use annotations on the Models to help with binding the Razor at the front of tha pplication 
     public class GuestResponse
     {
-        [Display(Name = "Your Name:")]
+        [Display(Name = "Your Name:") ]
+        [Required(ErrorMessage = "Your Name is Mandatory")]
         public string Name { get; set; }
 
         [Display(Name = "Your Email:")]
+        [Required(ErrorMessage = "Your Email address  is Mandatory")]
         public string Email { get; set; }
 
         [Display(Name = "Your Phone:")]
+        [Required(ErrorMessage = "Your Phone Number   is Mandatory")]
         public string Phone { get; set; }
 
         [Display(Name = "Will You Attend ? :")]
+        [Required(ErrorMessage = "Kindly Choose a selection")]
         public bool? willAtted { get; set; }
     }
 }
