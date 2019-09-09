6aea0d7bc554535d1ba33829bcd5dd33f25bbc92
Muyinda Rogers <moverr@gmail.com>
2019-09-09 14:32:24 +0200

    :Debugging :

--- partyInvites/Models/GuestResponse.cs
+++ partyInvites/Models/GuestResponse.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,9 +8,16 @@ namespace partyInvites.Models
 {
     public class GuestResponse
     {
+        [Display(Name = "Your Name:")]
         public string Name { get; set; }
+
+        [Display(Name = "Your Email:")]
         public string Email { get; set; }
+
+        [Display(Name = "Your Phone:")]
         public string Phone { get; set; }
+
+        [Display(Name = "Will You Attend ? :")]
         public bool? willAtted { get; set; }
     }
 }
