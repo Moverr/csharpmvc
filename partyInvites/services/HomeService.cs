87174754c8d26c9f4dca32b0cad14f5919ffab68
Muyinda Rogers <moverr@gmail.com>
2019-09-10 12:12:09 +0200

    Introducing my Service Architecture to the project

---
+++ partyInvites/services/HomeService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace partyInvites.services
+{
+    public class HomeService
+    {
+        private static HomeService homeService;
+        public HomeService()
+        {
+
+        }
+
+        public static HomeService getInstance()
+        {
+            if(homeService == null)
+            {
+                homeService = new HomeService();
+            }
+            return homeService;
+        }
+
+    }
+}
